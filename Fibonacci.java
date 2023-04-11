bc58d0a67014bede512b378d557dbda75f241156
C4rlloz <96665704+C4rlloz@users.noreply.github.com>
2023-04-10 21:41:01 -0300

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+public class Fibonacci {
+    
+    public static void main(String[] args){
+
+        int n1 = 1, n2 = 0;
+
+        System.out.println(n2);
+        System.out.println(n1);
+
+        for(int i = 0; i < 8; i++){
+            n1 = n1 + n2;
+            n2 = n1 - n2;
+            System.out.println(n1);
+        }
+    }
+}
+
